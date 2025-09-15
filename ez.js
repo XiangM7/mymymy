114912d0dc8d2c8720b5003cc9b53e1021c49782
XiangM7 <157428241+XiangM7@users.noreply.github.com>
2025-09-15 14:37:51 -0700

    Update and rename server.js to ez.js

--- ez.js
+++ ez.js
@@ -1,8 +1,9 @@
+
 const http = require('http');
 
 // 创建服务器
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('<h3>Hello World</h3>');
 });
 
