8dd27687b67bdeb77844844e726b804d05ca7292
XiangM7 <157428241+XiangM7@users.noreply.github.com>
2025-09-15 14:56:24 -0700

    Update ez.js

--- ez.js
+++ ez.js
@@ -1,13 +1,28 @@
-
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 // 创建服务器
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h3>Hello World</h3>');
+    // 当有人访问网站时，读取并发送 index.html 文件
+    if (req.url === '/') {
+        fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+            if (err) {
+                res.writeHead(500);
+                res.end('Error loading index.html');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(data);
+            }
+        });
+    } else {
+        // 处理其他页面请求（如果有）
+        res.writeHead(404);
+        res.end('Page not found');
+    }
 });
 
-// 启动服务器
+// 启动服务器在端口 80
 server.listen(80, () => {
     console.log('Server running at http://localhost:80/');
 });
