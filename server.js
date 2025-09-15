e702cc56866eafa313d6c875eb644073fff2967d
XiangM7 <157428241+XiangM7@users.noreply.github.com>
2025-09-15 14:33:03 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 // 创建服务器
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('<h1>Hello World</h1>');
+    res.end('<h3>Hello World</h3>');
 });
 
 // 启动服务器
